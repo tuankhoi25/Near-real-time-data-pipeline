3ff430e9519c56e3e22d780677e824378eb462cb
tuankhoi25 <tuankhoi.de@gmail.com>
2024-10-26 16:08:24 +0700

    Update airflow_dag.py

--- airflow/dags/airflow_dag.py
+++ airflow/dags/airflow_dag.py
@@ -12,15 +12,10 @@ with DAG(
     start_date = datetime(2024, 10, 25),
     schedule_interval = '0 7 * * *'
 ) as dag:
-    
+
     t1 = BashOperator(
-        task_id = "Data_generation",
-        bash_command = "cd ~ && cd airflow && cd py && echo 'Start generating data' && python3 faking_data.py && echo 'Data generation completed'",
-    )
-
-    t2 = BashOperator(
         task_id = "ETL",
-        bash_command = "cd ~ && cd airflow && cd py && echo 'Start ETL data' && python3 etl_process.py && echo 'ETL completed'",
+        bash_command = "cd ~ && cd airflow && cd py && echo 'Start ETL data' && python3 etl_process.py"
     )
 
-    t1 >> t2
+    t1
